1a05dbaf6df447b363afd53d3f1975e563383b40
Nicolás Morell <78945145+jnicomorell@users.noreply.github.com>
2025-08-20 20:48:59 -0300

    Add reservation invitations and mobile screens

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -13,6 +13,8 @@ Route::get('/fields', [FieldController::class, 'index']);
 Route::get('/fields/map', [FieldController::class, 'map']);
 Route::post('/fields', [FieldController::class, 'store'])->middleware('auth:sanctum');
 Route::get('/fields/{field}', [FieldController::class, 'show']);
+Route::put('/fields/{field}', [FieldController::class, 'update'])->middleware('auth:sanctum');
+Route::delete('/fields/{field}', [FieldController::class, 'destroy'])->middleware('auth:sanctum');
 
 Route::get('/reviews', [ReviewController::class, 'index']);
 Route::get('/reviews/{review}', [ReviewController::class, 'show']);
@@ -23,6 +25,10 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/reservations/{reservation}', [ReservationController::class, 'show']);
     Route::put('/reservations/{reservation}', [ReservationController::class, 'update']);
     Route::delete('/reservations/{reservation}', [ReservationController::class, 'destroy']);
+    Route::post('/reservations/{reservation}/pay', [ReservationController::class, 'pay']);
+    Route::post('/reservations/{reservation}/invite', [ReservationController::class, 'invite']);
+    Route::post('/reservations/{reservation}/confirm', [ReservationController::class, 'confirm']);
+    Route::get('/reservations/{reservation}/ics', [ReservationController::class, 'ics']);
     Route::post('/reviews', [ReviewController::class, 'store']);
     Route::put('/reviews/{review}', [ReviewController::class, 'update']);
     Route::delete('/reviews/{review}', [ReviewController::class, 'destroy']);
